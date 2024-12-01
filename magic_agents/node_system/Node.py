bad7a9bb83894cae34b0b178fd3b0a612c0afe3d
andres <andreslamosk124@hotmail.com>
2024-11-30 18:22:01 -0600

    Refactor add_parent method to simplify node type mapping
    The add_parent method is refactored to dynamically map node types to their keys using a streamlined loop, reducing redundancy and improving maintainability. Additionally, the method now automatically raises an exception if an unsupported node type is encountered, ensuring error handling is more reliable. Other minor improvements include automatic decoration of subclass call methods with telemetry for easier debugging.

--- magic_agents/node_system/Node.py
+++ magic_agents/node_system/Node.py
@@ -19,48 +19,24 @@ class Node(abc.ABC):
         }
 
     def add_parent(self, parent: dict, target: str):
-        print(self.__class__.__name__, parent)
+        # Define a mapping of possible node types to their keys
+        node_types = [
+            "NodeText", "NodeUserInput", "NodeParser",
+            "NodeChat", "NodeLLM", "NodeFetch"
+        ]
 
-        if c := parent.get('NodeText'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeUserInput'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeMerger'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeParser'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeChat'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeLLM'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeFindit'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeBrowsing'):
-            trg = {
-                target: c
-            }
-        elif c := parent.get('NodeFetch'):
-            trg = {
-                target: c
-            }
-        else:
-            raise Exception('Node target not supported', parent)
+        for node_type in node_types:
+            if content := parent.get(node_type):
+                self.parents.update({target: content})
+                return
 
-        self.parents.update(trg)
+        raise Exception("Node target not supported", parent)
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        # Automatically decorate the `__call__` method of the subclass
+        if '__call__' in cls.__dict__:
+            cls.__call__ = Node.magic_telemetry(cls.__call__)
 
     async def __call__(self, chat_log: ModelAgentRunLog):
         pass
@@ -72,10 +48,8 @@ class Node(abc.ABC):
     def magic_telemetry(func: Callable):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            # print("magic_telemetry:kwargs", kwargs)
-            # print("magic_telemetry:args", args)
+            print('Executing', func.__qualname__.split('.')[0])
             response = func(*args, **kwargs)
-            # print("magic_telemetry:response", response)
             return response
 
         return wrapper
