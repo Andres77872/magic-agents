b9c7851900a835ab9250e2d3ffdc702f7fef85e4
andres <andreslamosk124@hotmail.com>
2024-12-02 02:26:48 -0600

    Refactor Node class for enhanced functionality
    Introduce a node_id attribute and refactor the response handling by storing and checking it before processing in the `__call__` method to improve performance and clarity. Additionally, streamline `add_parent` method by simplifying the update logic and modify the subclass method decoration to apply to `process` instead of `__call__`. These changes enhance the maintainability and extendability of the Node class.

--- magic_agents/node_system/Node.py
+++ magic_agents/node_system/Node.py
@@ -6,38 +6,45 @@ from magic_agents.util.telemetry import magic_telemetry
 class Node(abc.ABC):
     def __init__(self,
                  cost=0,
+                 node_id=None,
                  debug: bool = False,
                  **kwargs):
         self.cost = cost
         self.parents = {}
         self.debug = debug
+        self.response = None
+        self.node_id = node_id
 
     def prep(self, content):
+        self.response = content
         return {
-            self.__class__.__name__: content
+            'node': self.__class__.__name__,
+            'content': content
         }
 
-    def add_parent(self, parent: dict, target: str):
-        # Define a mapping of possible node types to their keys
-        node_types = [
-            "NodeText", "NodeUserInput", "NodeParser",
-            "NodeChat", "NodeLLM", "NodeFetch", "NodeClientLLM"
-        ]
-
-        for node_type in node_types:
-            if content := parent.get(node_type):
-                self.parents.update({target: content})
-                return
-
-        raise Exception("Node target not supported", parent)
+    def add_parent(self, parent: dict, source: str, target: str):
+        self.parents.update({target: parent['content']})
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        # Automatically decorate the `__call__` method of the subclass
-        if '__call__' in cls.__dict__:
-            cls.__call__ = magic_telemetry(cls.__call__)
+        # Automatically decorate the `process` method of the subclass
+        if 'process' in cls.__dict__:
+            cls.process = magic_telemetry(cls.process)
 
     async def __call__(self, chat_log: ModelAgentRunLog):
+        # Check if the response is already computed
+        if self.response is not None:
+            yield {
+                'type': 'end',
+                'content': self.prep(self.response)
+            }
+        else:
+            # Execute the subclass-specific logic
+            async for result in self.process(chat_log):
+                yield result
+
+    @abc.abstractmethod
+    async def process(self, chat_log: ModelAgentRunLog):
         pass
 
     def get_debug(self):
